440118c3b3195a26f9d38a860d3a91eb510df511
Californiae <1288217@ukr.net>
2024-07-29 08:39:25 +0200

    refactor: apply prettier

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let res=''
+    n=String(Math.abs(n))  
+    l=n.length
+    
+    for (let i=l-1; i>=0; i--)
+    {res=res+n.slice(i,i+1)
+        console.log(i ,n.slice(i,i+1) )
+    }
+    return Number(res)
 }
